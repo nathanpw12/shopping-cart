f0bef53a10467312834655912e0b838f862ce036
Nathan Vitor <nathanvitor38@gmail.com>
2022-06-09 09:57:50 -0300

    Requisito 1:  Aprovado pelo Cypress.

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -1,8 +1,15 @@
 require('../mocks/fetchSimulator');
+const { type } = require('mocha/lib/utils');
 const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  
+  it('Teste se fetchProducts é uma função.', () => {
+    expect(fetchProducts).toBeInstanceOf(Function);
+  });
+
+  it('Execute a função fetchProducts com o argumento "computador" e teste se fetch foi chamada', () => {
+    expect(fetchProducts('computador')).toBeCalled;
+  })
 });
