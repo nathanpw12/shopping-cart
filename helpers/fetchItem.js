148a4239efda04eae9073c9f38b9a90043c662d0
Nathan Vitor <nathanvitor38@gmail.com>
2022-07-02 18:01:29 -0300

    Requisito 3:  Aprovado Localmente

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,12 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (item) => {
+  const URL = `https://api.mercadolibre.com/items/${item}`;
+  try {
+    const wantedData = await fetch(URL);
+    const results = await wantedData.json();
+    return results;
+  } catch (error) {
+  return error; 
+  }
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -15,8 +15,8 @@ describe('2 - Teste a função fetchItem', () => {
 
   it('Teste se, ao chamar a função fetchItem com o argumento do item "MLB1615760527", a função fetch utiliza o endpoint "https://api.mercadolibre.com/items/MLB1615760527";', async () => {
     const URL = 'https://api.mercadolibre.com/items/MLB1615760527';
-    const retornoFetchItem = await fetchItem('MLB1615760527');
-    expect(retornoFetchItem).toHaveBeenCalledWith(URL);
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledWith(URL);
   });
 
   it('Teste se o retorno da função fetchItem com o argumento do item "MLB1615760527" é uma estrutura de dados igual ao objeto item que já está importado no arquivo.', async () => {
