cbd45da89f216dc9e5b83b3a8fcd1e2b1b5309ef
Nathan Vitor <nathanvitor38@gmail.com>
2022-07-02 17:24:36 -0300

    Requisito 1:  Aprovado Localmente

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,15 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async (produto) => {
+  try {
+    const URL = `https://api.mercadolibre.com/sites/MLB/search?q=${produto}`;
+    const response = await fetch(URL);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
 };
+
+fetchProducts();
 
 if (typeof module !== 'undefined') {
   module.exports = {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -9,7 +9,23 @@ describe('1 - Teste a função fetchProducts', () => {
     expect(fetchProducts).toBeInstanceOf(Function);
   });
 
-  it('Execute a função fetchProducts com o argumento "computador" e teste se fetch foi chamada', () => {
-    expect(fetchProducts('computador')).toBeCalled;
-  })
+  it('Se a função fetchProducts ter como argumento "computador", teste se fetch foi chamada', () => {
+    expect(fetchProducts('computador')).toBeCalled; 
+  });
+
+  it('Teste se, ao chamar a função fetchProducts com o argumento "computador", a função fetch utiliza o endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computador"', async () => {
+    const URL = 'https://api.mercadolibre.com/sites/MLB/search?q=computador'
+    await fetchProducts('computador');
+    expect(fetch).toBeCalledWith(URL);
+  });
+
+  it('Teste se o retorno da função fetchProducts com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch, que já está importado no arquivo.', async () => {
+    const retornoFetchProducts = await fetchProducts('computador');
+    expect(retornoFetchProducts).toEqual(computadorSearch);
+  });
+
+  it('Teste se, ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: "You must provide an url"', async() => {
+    const retornoFetchProducts = await fetchProducts();
+    expect(retornoFetchProducts).toEqual(new Error('You must provide an url'));
+  });
 });
